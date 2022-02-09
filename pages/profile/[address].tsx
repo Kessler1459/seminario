78dd2bfedbcaccf66cc1062f0d196a4b31921b75
Kessler1459 <kessler1459@gmail.com>
2022-02-09 17:07:13 -0300

    Update [address].tsx

--- pages/profile/[address].tsx
+++ pages/profile/[address].tsx
@@ -6,25 +6,26 @@ import { isAddress } from "ethers/lib/utils";
 import User from "../../models/user";
 import Degree from "../../models/degree";
 import Course from "../../models/course";
+import Card from "../../components/degree-card/card";
 
 const Profile = () => {
 	const router = useRouter();
 	const { contract } = useEthersContext();
 	const { getFullUser, loading } = useUsers(contract);
 	const [user, setUser] = useState<User | null>(null);
-    const [latestCourse,setLatestCourse]=useState<Course|null>(null)
+	const [latestCourse, setLatestCourse] = useState<Course | null>(null);
 
 	useEffect(() => {
 		const add = router.query.address;
 		if (!user && typeof add === "string" && isAddress(add)) {
 			getFullUser(add).then((us) => {
 				setUser(us);
-                getLatestCourse(us);
+				getLatestCourse(us);
 			});
 		}
 	}, [router.query.address]);
 
-	const getLatestCourse = (user:User) => {
+	const getLatestCourse = (user: User) => {
 		let latest: Course | null = null;
 		if (user?.degrees) {
 			for (const degree of user.degrees) {
@@ -34,9 +35,15 @@ const Profile = () => {
 				}
 			}
 		}
-        setLatestCourse(latest);
+		setLatestCourse(latest);
 	};
 
+	const getTotalAverage = () => {
+        const sum=user?.degrees.reduce((s, c) => s + c.courses.reduce((sum, val) => sum + val.score, 0) / c.courses.length, 0)
+        if(sum && user?.degrees)
+            return (sum/user?.degrees.length).toFixed(2);
+	};
+	
 	return (
 		<>
 			<h2>{user?.address}</h2>
@@ -47,7 +54,10 @@ const Profile = () => {
 				<>
 					<div>
 						<i>Latest token</i>
-						<p>#{latestCourse?.tokenId.toString()} Earned {latestCourse?.name} on {latestCourse?.date}</p>
+						<p>
+							#{latestCourse?.tokenId.toString()} Earned {latestCourse?.name} on{" "}
+							{latestCourse?.date.toLocaleDateString()}
+						</p>
 					</div>
 					<ul>
 						<li>Tokens: {user?.degrees.reduce<number>((acum, cur) => acum + cur.courses.length, 0)}</li>
@@ -55,7 +65,11 @@ const Profile = () => {
 							Total score:{" "}
 							{user?.degrees.reduce<number>((sum, cur) => sum + cur.courses.reduce((s, c) => s + c.score, 0), 0)}
 						</li>
+                        <li>Average: {getTotalAverage()}</li>
 					</ul>
+					{user?.degrees.map((deg, i) => (
+						<Card key={i} degree={deg} />
+					))}
 				</>
 			)}
 		</>
